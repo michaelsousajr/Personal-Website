8f32f36861f0c03aff654aa4895b730468939d8c
Michael <89663481+michaelsousajr@users.noreply.github.com>
2023-11-07 09:50:10 -0500

    added imgs, update pages

---
+++ components/logo.js
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+import Image from 'next/image'; // Changed 'next/images' to 'next/image'
+import { Text, useColorModeValue } from '@chakra-ui/react';
+import styled from '@emotion/styled';
+
+const LogoBox = styled.span`
+  font-weight: bold;
+  font-size: 18px;
+  display: inline-flex;
+  align-items: center;
+  height: 30px;
+  line-height: 20px;
+  padding: 10px;
+
+  &:hover img {
+    transform: rotate(20deg);
+  }
+`;
+
+const Logo = () => {
+  const coffeeImg = `/images/coffee${useColorModeValue('', '-dark')}.png`; // Use backticks for string interpolation
+  return (
+    <Link href="/">
+      <a>
+        <LogoBox>
+          <Image src={coffeeImg} width={20} height={20} alt="logo" />
+          <Text color={useColorModeValue('gray.800', 'whiteAlpha.900')} fontFamily='M PLUS Rounded 1c' fontWeight="bold" ml={3}>
+            Michael Sousa
+          </Text>
+        </LogoBox>
+      </a>
+    </Link>
+  );
+};
+
+export default Logo;
+
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
 import '@/styles/globals.css'
+import { ChakraProvider } from '@chakra-ui/react'
+import Layout from '../components/layouts/main'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function Website({ Component, pageProps, router }) {
+  return (
+    <ChakraProvider>
+      <Layout>
+        <Component {...pageProps} key={router.route} />
+      </Layout>
+      
+    </ChakraProvider>
+  )
 }
+
